4f67e29365722f89d86cb713988adbb30bcecc47
EfectoNegocioLuisE <luis.rodriguez@efectonegocio.com>
2020-05-22 04:10:31 -0500

    Campos globales Terminados

--- cotizador__airovac/models/sale.py
+++ cotizador__airovac/models/sale.py
@@ -11,7 +11,7 @@ class SaleOrderInherit(models.Model):
     e_etiqueta_title_b = fields.Text(string="Titulo de Etiqueta B")
     e_desciption = fields.Char(string="Descripción")
 
-    e_g_m_p = fields.Monetary(compute='_compute_e_g_m_p',string="G.M. del Proyecto",readonly="True")
+    e_g_m_p = fields.Float(digits=(1, 3),Default = 0,compute='_compute_e_g_m_p',string="G.M. del Proyecto",readonly="True")
     e_costo_total_obra = fields.Monetary(compute='_compute_e_costo_total_obra',string="Costo Total Obra",readonly="True")
     e_costo_total_imp_obra = fields.Monetary(compute='_compute_e_costo_total_imp',string="Costo Total Imp Obra",readonly="True")
 
@@ -253,8 +253,8 @@ class SaleOrderLineInherit(models.Model):
     _inherit = 'sale.order.line'
 
 
-    e_igi = fields.Float(digits=(3, 2), string="IGI %",help="Porcentaje de IGI")
-    e_importation = fields.Float(digits=(3, 2), string="IMPOR %",help="Porcentaje de Importación")
+    e_igi = fields.Float(digits=(1, 4), string="IGI %",help="Porcentaje de IGI")
+    e_importation = fields.Float(digits=(1, 4), string="IMPOR %",help="Porcentaje de Importación")
     e_etiqueta_line_a = fields.Text(string="Etiqueta A")
     e_etiqueta_line_b = fields.Text(string="Etiqueta B")
     e_te_line_max = fields.Integer(string="T.E MIN")
@@ -262,14 +262,14 @@ class SaleOrderLineInherit(models.Model):
     e_precio_de_lista = fields.Float(digits=(10, 2),readonly=True,string="P . L", help="Precio de lista")
     e_multiplicador = fields.Float(digits=(1, 2),default=0, string="Multiplicador", help="Multiplicador, si no exite 1")
     e_descuento = fields.Integer(string="Des %")
-    price_unit =fields.Float(digits=(3, 2),readonly=True, string="Punto de Venta",help="P.L * Multiplicador * (1 - Descuento)")
+    price_unit =fields.Float(digits=(10, 2),readonly=True, string="Punto de Venta",help="P.L * Multiplicador * (1 - Descuento)")
     e_costo_total =fields.Monetary(string="Costo Total",readonly=True)
 
-    e_costo_unitario = fields.Monetary(digits=(1, 2),Default = 0,store=True,readonly=True, string="Costo Unitario", help="(1 + IGI + Impotación) * (PL * Mult. STD)")
-    e_costo_total = fields.Float(digits=(1, 2),Default = 0, store=True,readonly=True, string="Costo Total", help="Costo Unitario * Cantidad")
-    e_costo_total_imp = fields.Float(digits=(1, 2),Default = 0, store=True,readonly=True, string="C . T Import", help="Importacion * (PL * Mult. STD) * Cantidad")
-    e_g_m_l = fields.Float(digits=(1, 2),Default = 0, store=True,readonly=True, string="G . M ", help="COSTO TOTAL / Subtotal")
-    e_estimado_pro_l = fields.Float(digits=(1, 2), Default=0,readonly = True, store=True, string="S.T.P %", help="% Sobre total de propuesta")
+    e_costo_unitario = fields.Monetary(digits=(1, 3),Default = 0,store=True,readonly=True, string="Costo Unitario", help="(1 + IGI + Impotación) * (PL * Mult. STD)")
+    e_costo_total = fields.Float(digits=(1, 3),Default = 0, store=True,readonly=True, string="Costo Total", help="Costo Unitario * Cantidad")
+    e_costo_total_imp = fields.Float(digits=(1, 3),Default = 0, store=True,readonly=True, string="C . T Import", help="Importacion * (PL * Mult. STD) * Cantidad")
+    e_g_m_l = fields.Float(digits=(1, 3),Default = 0, store=True,readonly=True, string="G . M ", help="COSTO TOTAL / Subtotal")
+    e_estimado_pro_l = fields.Float(digits=(1, 3), Default=0,readonly = True, store=True, string="S.T.P %", help="% Sobre total de propuesta")
     e_asociar = fields.Boolean( Default=False,string="asociar",help="Asocia productos con accesorios cada dos checkboxes")
     e_p_unit_a = fields.Monetary(Default=0,readonly=True,string="P.U con Accesorios",help="Precio unitario con accesorios")
     e_subtotal_no_des = fields.Monetary(Default=0,string="Subtutal",help="Sub tutal no desglosado")
@@ -289,6 +289,8 @@ class SaleOrderLineInherit(models.Model):
     e_mult_std = fields.Float(digits=(1, 2),default = 1.0, string="Mult std",
                               help="Exwork mult")
     e_mult_min = fields.Float(digits=(1, 2),default = 1, string="Mult. Min", help="e_mult_min")
+
+    e_exwork = fields.Monetary( string="Cost Exwork",compute = '_compute_exwor', help="e_mult_min")
 
     @api.onchange('e_costo_unitario', 'product_uom_qty')
     def compute_costo_total(self):
@@ -444,20 +446,32 @@ class SaleOrderLineInherit(models.Model):
             )
             self.price_unit = self.env['account.tax']._fix_tax_included_price_company(price, product.taxes_id, self.tax_id, self.company_id)
 
+    @api.onchange('e_mult_std')
+    def _compute_exwor(self):
+        if self.e_mult_std < 0:
+            self.e_mult_std = self._origin.e_mult_std
+            self.e_exwork = self.e_mult_std * self.e_precio_de_lista
+            return {
+                'warning': {
+                    'title': "Cuidado",
+                    'message': "El multiplicador solicitado no puede ser negativo",
+                    'type': 'notification'
+                }
+            }
+        self.e_exwork = self.e_mult_std * self.e_precio_de_lista
 
 
-
-    @api.onchange('product_id','e_igi','e_importation','e_mult_std')
+    @api.onchange('product_id','e_igi','e_importation','e_exwork')
     def compute_costo_unitario(self):
-        self.e_costo_unitario =  (1 + self.e_igi + self.e_importation) * ( self.e_mult_std * self.e_precio_de_lista)
+        self.e_costo_unitario =  (1 + self.e_igi + self.e_importation) *  self.e_exwork
 
     @api.onchange('e_costo_unitario','product_uom_qty')
     def compute_costo_total(self):
         self.e_costo_total = ( self.e_costo_unitario * self.product_uom_qty)
 
-    @api.onchange('e_importation','product_uom_qty','e_mult_std')
+    @api.onchange('e_importation','product_uom_qty','e_exwork')
     def compute_costo_total_imp(self):
-        self.e_costo_total_imp = self.e_importation * (self.e_precio_de_lista * self.e_mult_std) * self.product_uom_qty
+        self.e_costo_total_imp = self.e_importation * self.e_exwork  * self.product_uom_qty
 
     @api.onchange('e_costo_total','price_unit','product_uom_qty')
     def compute_g_m_l(self):
@@ -465,12 +479,6 @@ class SaleOrderLineInherit(models.Model):
             return 0
         self.e_g_m_l = self.e_costo_total / (self.price_unit * self.product_uom_qty)
 
-    @api.onchange('e_costo_total', 'price_unit', 'product_uom_qty')
-    def compute_g_m_l(self):
-        if self.e_costo_total == 0 or self.price_unit == 0 or self.product_uom_qty == 0:
-            return 0
-        self.e_g_m_l = self.e_costo_total / (
-                    self.price_unit * self.product_uom_qty)
 
     @api.onchange('e_provedor')
     def _onchange_e_mult_std(self):
