608555795a56df51c4751570621ca596d0ba5471
EfectoNegocioLuisE <luis.rodriguez@efectonegocio.com>
2020-05-31 23:14:19 -0500

    cambios en numero de decimales, productos y multiplicadores 4 decimales

--- cotizador__airovac/models/product.py
+++ cotizador__airovac/models/product.py
@@ -7,16 +7,16 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 class productTemplateInherit(models.Model):
     _inherit = 'product.template'
 
-    e_igi = fields.Float(digits=(3, 2),string="IGI%", help="Porcentaje de IGI")
-    e_importation = fields.Float(digits=(3, 2),string="STD Importación%", help="Porcentaje de Importación")
+    e_igi = fields.Float(digits=(3, 4),string="IGI%", help="Porcentaje de IGI")
+    e_importation = fields.Float(digits=(3, 4),string="STD Importación%", help="Porcentaje de Importación")
     #e_precio_list = fields.Float(digits=(10,2), string="Precio de Lista", help="Precio de lista")
     e_te_max = fields.Integer(string="T.E MIN")
     e_te_min = fields.Integer(string="T.E MAX")
     e_etiqueta_a = fields.Text(string="Etiqueta A")
     e_etiqueta_b = fields.Text(string="Etiqueta B")
-    e_mult_min = fields.Float(digits=(1, 4),default = 1, string="Mult. Min", help="e_mult_min")
+    e_mult_min = fields.Float(digitgits=(10, 4),default = 1, string="Mult. Min", help="e_mult_min")
     e_precio_de_lista = fields.Float(digits=(10, 2), string="Precio de Lista", help="Precio de lista")
-    e_product_class  = fields.Many2one('producte.class',
+    e_product_class  = fields.Many2one('prgit soducte.class',
                                          string="Clase de producto",
                                          )
     e_tiempo_estimado = fields.Char(string="Tiempo Estimado",
