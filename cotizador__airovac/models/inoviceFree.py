9f7ddfc816edcd87009de3b8420db2eeda2194b9
EfectoNegocioLuisE <luis.rodriguez@efectonegocio.com>
2020-08-12 20:47:26 -0500

    readonly = 0 l10n_mx_edi_cfdi_name

---
+++ cotizador__airovac/models/inoviceFree.py
@@ -0,0 +1,29 @@
+import base64
+from itertools import groupby
+import re
+import logging
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from io import BytesIO
+import requests
+from pytz import timezone
+
+from lxml import etree
+from lxml.objectify import fromstring
+from zeep import Client
+from zeep.transports import Transport
+
+from odoo import _, api, fields, models, tools
+from odoo.tools.xml_utils import _check_with_xsd
+from odoo.tools import DEFAULT_SERVER_TIME_FORMAT
+from odoo.tools import float_round
+from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_repr
+
+from odoo.addons.l10n_mx_edi.tools.run_after_commit import run_after_commit
+
+class AccountMoveFree(models.Model):
+    _inherit = 'account.move'
+
+    l10n_mx_edi_cfdi_name = fields.Char(string='CFDI name', copy=False,
+                                        help='The attachment name of the CFDI editado.')
