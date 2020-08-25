02d58035fd117609d1a4a2b0dbfb84eb58bfc99c
EfectoNegocioLuisE <luis.rodriguez@efectonegocio.com>
2020-08-25 18:26:09 -0500

    fecha de pedimentos account.move.line

--- cotizador__airovac/models/inoviceFree.py
+++ cotizador__airovac/models/inoviceFree.py
@@ -22,8 +22,34 @@ from odoo.tools.float_utils import float_repr
 
 from odoo.addons.l10n_mx_edi.tools.run_after_commit import run_after_commit
 
+class EfectoAccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    efecto_mx_edi_customs_number = fields.Char(
+        string='Fechas de los numeros de pedimento')
+
+
 class AccountMoveFree(models.Model):
     _inherit = 'account.move'
 
     l10n_mx_edi_cfdi_name = fields.Char(string='CFDI name', copy=False, readonly=False,
                                         help='The attachment name of the CFDI editado.')
+
+
+    def post(self):
+        # OVERRIDE PARA FECHAS
+        for move in self.filtered(lambda move: move.is_invoice()):
+            for line in move.line_ids:
+                stock_moves = line.mapped('sale_line_ids.move_ids').filtered(lambda r: r.state == 'done' and not r.scrapped)
+                if not stock_moves:
+                    continue
+                landed_costs = self.env['stock.landed.cost'].sudo().search([
+                    ('picking_ids', 'in', stock_moves.mapped('move_orig_fifo_ids.picking_id').ids),
+                    ('l10n_mx_edi_customs_number', '!=', False),
+                ])
+                if not landed_costs:
+                    continue
+                line.l10n_mx_edi_customs_number = ','.join(list(set(landed_costs.mapped('l10n_mx_edi_customs_number'))))
+                line.efecto_mx_edi_customs_number = ','.join(list(set(landed_costs.mapped('date'))))
+        super(AccountMoveFree, self).post()
+
