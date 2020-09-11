703d6a54b980fdee6d5b8d3fd98dd53a42bfc487
EfectoNegocioLuisE <luis.rodriguez@efectonegocio.com>
2020-09-11 02:55:22 -0500

    efecto_payment type

--- efecto_patment_type_mx/models/models.py
+++ efecto_patment_type_mx/models/models.py
@@ -7,5 +7,5 @@ from odoo.addons.crm.models.digest import Digest
 class efecto_patment_type(models.Model):
     _inherit = 'account.payment'
 
-    efecto_payment_type = fields.Float(default=0,digits=10.4,string="Tipo de cambio")
+    efecto_payment_type = fields.Float(digits=(1, 4),default=1, string="tipo de cambio", help="tipo de cambio")
 
